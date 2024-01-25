346b209e6461086adc226fcab12726fc7a001545
Zach Affron <carolanbrian77@yahoo.ie>
2024-01-25 10:16:18 +0000

    Comment on HelloServelt
    Joke comment about sean, I actually love him no homo

--- XtraVision/src/main/java/com/example/xtravision/HelloServlet.java
+++ XtraVision/src/main/java/com/example/xtravision/HelloServlet.java
@@ -23,7 +23,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
 
-        //sean is an asshole he wont stop looking over my shoulder
+        //sean is an asshole he wont stop looking over my shoulder x2
 
     }
 
