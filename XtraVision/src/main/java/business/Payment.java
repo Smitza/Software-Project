988689a45575601cde421ed51a6d4b7032dc5613
Smitza <146719967+Smitza@users.noreply.github.com>
2024-01-26 13:42:15 +0000

    Added basic structure to payment

--- XtraVision/src/main/java/business/Payment.java
+++ XtraVision/src/main/java/business/Payment.java
@@ -1,4 +1,41 @@
 package business;
 
+import java.util.Date;
+
 public class Payment {
+
+    private String paymentId;
+    private double amount;
+    private Date paymentDate;
+    private String paymentMethod; //Methods could be credit card,  paypal,  google pay or  voucher
+
+    // Constructors
+    public Payment() {
+    }
+
+    public Payment(String paymentId, double amount, Date paymentDate, String paymentMethod) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.paymentMethod = paymentMethod;
+    }
+
+    // Getters and Setters
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+
 }
