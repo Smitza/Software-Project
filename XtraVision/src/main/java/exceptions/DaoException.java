7056d82ae9e8787f9f4e5ef1cbe08870c9ad21d5
OranBrady <oranbrady01@gmail.com>
2024-02-07 22:10:50 +0000

    More Javadoc

--- XtraVision/src/main/java/exceptions/DaoException.java
+++ XtraVision/src/main/java/exceptions/DaoException.java
@@ -2,20 +2,42 @@ package exceptions;
 
 import java.sql.SQLException;
 
+/**
+ * Represents an exception specific to DAO operations.
+ */
 public class DaoException extends SQLException {
 
+    /**
+     * Default constructor.
+     */
     public DaoException() {
         super();
     }
 
+    /**
+     * Constructor with a message.
+     *
+     * @param message Error message.
+     */
     public DaoException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor with a message and cause.
+     *
+     * @param message Error message.
+     * @param cause   Cause of the exception.
+     */
     public DaoException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructor with a cause.
+     *
+     * @param cause Cause of the exception.
+     */
     public DaoException(Throwable cause) {
         super(cause);
     }
