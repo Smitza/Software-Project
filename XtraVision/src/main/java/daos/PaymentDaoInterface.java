3e8b308abdfa99af9b1702c46af04d811da68daa
Zach Affron <carolanbrian77@yahoo.ie>
2024-02-07 18:54:49 +0000

    AddPayment(Payment payment): Adds a payment to the payment table.
    deletePaymentById(int pId):
    deletes a payment record off the table based off the supplied Id.
    
    getPaymentById(int pId):
    gets a payment record from the payment table based off the id supplied
    
    updatePayment(Payment P):
    Updates the information of the payment record in the payment table based off the supplied payment object
    
    getPaymentByDateRange(LocalDate startDate, LocalDate endDate):
    Gets an array list of payments based off the supplied start and end dates
    
    getPaymentsByMethod(String Method):
    gets payment records based on what they payed with (visa, mastercard, etc..)
    
    getAllPayments(): gets all payment records

--- XtraVision/src/main/java/daos/PaymentDaoInterface.java
+++ XtraVision/src/main/java/daos/PaymentDaoInterface.java
@@ -1,4 +1,24 @@
 package daos;
+import business.Payment;
+import exceptions.DaoException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public interface PaymentDaoInterface {
+
+    public boolean addPayment(Payment p) throws DaoException;
+
+    public boolean deletePaymentById(int pId) throws DaoException;
+
+    public int getPaymentById(int pId) throws DaoException;
+
+    public boolean updatePayment(Payment p) throws  DaoException;
+
+    public ArrayList<Payment> getPaymentByDateRange(LocalDate startDate, LocalDate endDate) throws DaoException;
+
+    public ArrayList<Payment> getPaymentByMethod(String method) throws DaoException;
+
+    public ArrayList<Payment> getAllPayments() throws DaoException;
+
 }
